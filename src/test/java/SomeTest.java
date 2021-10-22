dea128d3c601368049ef6f112ceea082b740344d
Yaroslava Grats <47428996+yasyaG@users.noreply.github.com>
2021-10-22 12:58:15 -0700

    Update SomeTest.java

--- src/test/java/SomeTest.java
+++ src/test/java/SomeTest.java
@@ -2,6 +2,7 @@
 package net.codejava;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class SomeTest extends TestFormApplication {
    @Test public void test1() {
@@ -11,4 +12,20 @@ public class SomeTest extends TestFormApplication {
    @Test public void test2() {
       run();
    }
+  
+  
+   // test the setUserName
+   @Test public void testSetName() {
+      testUser.setName("Norma");
+      String thisTestName = testUser.getName();
+      assertEquals("Norma", thisTestName);
+   }
+
+   // test the setGender
+   @Test public void testSetGender() {
+      testUser.setGender("Female");
+      String thisTestGender = testUser.getGender();
+      assertEquals("Female", thisTestGender);
+   }
+
 }
